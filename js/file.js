baa49dbe0df2d31ab0f866c3ea033f813a6e5ccd
mh610creative <nonsensemh1@gmail.com>
2021-02-01 11:09:06 -0800

    daily commit. added borders in CSS and review,add pseudo for JS steps of page

---
+++ js/file.js
@@ -0,0 +1,33 @@
+// Page loads, DOM loads, User reads page
+document.addEventListener('DOMContentLoaded', console.log ('sanity check'))
+
+
+
+// User enters Form Data, types in form
+
+
+
+
+// User clicks Submit button
+
+
+
+
+
+// check for above, Form disappears
+
+
+
+
+// Form is replaced by GIFs pulled by API,Fetch AJAX Data here
+
+
+
+
+
+
+// User clicks other links on page maybe
+
+
+
+
